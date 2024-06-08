72547d9a7c03986367aaab843a31159202d905e9
Caleb Johnson-Cantrell <46861012+calebrjc@users.noreply.github.com>
2024-06-07 23:36:47 -0500

    chore: Set up the project (#1)

---
+++ build.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const sge = b.dependency("sge", .{ .target = target, .optimize = optimize });
+
+    const exe = b.addExecutable(.{
+        .name = "asteroids",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("sge", sge.module("sge"));
+    b.installArtifact(exe);
+
+    // "zig build run" -----------------------------------------------------------------------------
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
